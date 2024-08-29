db579e7b888c8c0fac0b65d043b7523abf9ce9c5
collidingScopes <166117863+collidingScopes@users.noreply.github.com>
2024-08-29 16:31:58 +0800

    Change background color

--- ASCII.js
+++ ASCII.js
@@ -4,7 +4,6 @@ Allow toggle for different monospace fonts (Japanese, bolder font, etc.)
 Allow image upload, with function to determine based on the file extension and handle accordingly
 When image is uploaded, use a new function that accepts the image and then runs scanLines algo on it -- as pseudo video feed
 Investigate frame rate unsynced issue when video recording -- video export can have dropped frames / uneven time / low quality
-On mobile -- default video is too wide? Need to resize video or screen upon startup?
 Allow custom char set during Random Text mode (so that you can try with only a few chars)
 */
 
